538f16b9acabeed19a3278b3ffdf300ea5f38922
Damien Kick <damien.kick@vallen.com>
2025-08-20 16:57:46 -0500

    The ol' forgot to add the new file trick

---
+++ src/dkick/clj_sql_parser/statement/select/from_item/joins.clj
@@ -0,0 +1,111 @@
+(ns dkick.clj-sql-parser.statement.select.from-item.joins
+  (:require
+   [honey.sql.helpers :as sqh])
+  (:import
+   (net.sf.jsqlparser.schema Table)
+   (net.sf.jsqlparser.statement.select Join ParenthesedSelect)))
+
+(defmulti visit-join
+  (fn [_from-visitor sql-parsed]
+    (type sql-parsed)))
+
+(defmethod visit-join Object [that sql-parsed]
+  (let [context (atom [])]
+    (.accept sql-parsed that context)
+    @context))
+
+(defmethod visit-join ParenthesedSelect [that sql-parsed]
+  (let [context (atom [])
+        alias   (some-> sql-parsed .getAlias .getName keyword)]
+    (.accept (.getSelect sql-parsed) (.getSelectVisitor that) context)
+    (if-not alias
+      @context
+      [(conj @context alias)])))
+
+(defmethod visit-join Table [_ sql-parsed]
+  (let [x-name (keyword (.getFullyQualifiedName sql-parsed))
+        alias  (.getAlias sql-parsed)]
+    [(if-not alias
+       x-name
+       [x-name (-> alias .getName keyword)])]))
+
+(defn join-data [x]
+  (sort
+   {:on-expressions (.getOnExpressions x)
+    :using-columns  (.getUsingColumns x)
+    :outer          (.isOuter x)
+    :right          (.isRight x)
+    :left           (.isLeft x)
+    :natural        (.isNatural x)
+    :global         (.isGlobal x)
+    :full           (.isFull x)
+    :inner          (.isInner x)
+    :simple         (.isSimple x)
+    :cross          (.isCross x)
+    :semi           (.isSemi x)
+    :straight       (.isStraight x)
+    :apply          (.isApply x)
+    :fromItem       (.getFromItem x)}))
+
+(defn type-of-join [sql-parsed]
+  (cond (.isOuter sql-parsed) :outer
+              (.isRight sql-parsed) :right
+              (.isFull sql-parsed)  :full
+              (.isLeft sql-parsed)  :left
+              (.isInner sql-parsed) :inner
+
+              (not-any? #(% sql-parsed)
+                        [Join/.isApply
+                         Join/.isCross
+                         Join/.isFull
+                         Join/.isGlobal
+                         Join/.isInner
+                         Join/.isLeft
+                         Join/.isNatural
+                         Join/.isOuter
+                         Join/.isRight
+                         Join/.isSemi
+                         Join/.isSimple
+                         Join/.isStraight])
+              :join
+
+              :else
+              (throw
+               (ex-info
+                "Unknown type of join"
+                {:join-object sql-parsed
+                 :join-data   (join-data sql-parsed)}))))
+
+(defmethod visit-join Join [that sql-parsed]
+  (let [what-to-join (visit-join that (.getFromItem sql-parsed))
+
+        what-to-join
+        (if-not (seq (.getUsingColumns sql-parsed))
+          what-to-join
+          (let [context (atom [])]
+            (doseq [x (.getUsingColumns sql-parsed)]
+              (.accept x (.getExpressionVisitor that) context))
+            (conj what-to-join `[:using ~@(deref context)])))
+
+        what-to-join
+        (if-not (seq (.getOnExpressions sql-parsed))
+          what-to-join
+          (let [context (atom [])]
+            (doseq [x (.getOnExpressions sql-parsed)]
+              (.accept x (.getExpressionVisitor that) context))
+            (apply conj what-to-join @context)))]
+
+    [(type-of-join sql-parsed) what-to-join]))
+
+(defn visit-joins [that joins context]
+  (doseq [[x-joins y-joins]
+          (->> (partition-by #(.isSimple %) joins)
+               (partition 2 1 nil))]
+    (assert (or (nil? y-joins) (not (.isSimple (first y-joins)))))
+    (if (.isSimple (first x-joins))
+      (doseq [x x-joins]
+        (-> x .getFromItem (.accept that context)))
+      (->> x-joins
+           (mapcat #(visit-join that %))
+           (apply sqh/join-by)
+           (swap! context conj)))))
